95c11c085ab0498ea4c59960d139700fe00a48bd
Jorge Escobar <jorge@knewton.com>
2016-12-12 13:21:12 -0500

    small issue with stores api

--- store/api.py
+++ store/api.py
@@ -32,7 +32,7 @@ class StoreAPI(MethodView):
                 return jsonify({}), 404
         else:
             stores = Store.objects.filter(live=True)
-            page = int(request.args.get('page', self.STORES_PER_PAGE))
+            page = int(request.args.get('page', 1))
             stores = stores.paginate(page=page, per_page=self.STORES_PER_PAGE)
             response = {
                 "result": "ok",
