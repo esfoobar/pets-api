29d2a5326c1cc51d7d211afb6db97a0d3ef8168c
Jorge Escobar <j#jorge3.com>
2017-01-01 22:01:28 -0500

    started pet tests

---
+++ pet/tests.py
@@ -0,0 +1,95 @@
+from application import create_app as create_app_base
+from mongoengine.connection import _get_db
+import unittest
+import json
+
+from settings import MONGODB_HOST, MONGODB_DB
+from pet.models import Pet
+from application import fixtures
+
+class PetTest(unittest.TestCase):
+
+    def create_app(self):
+        self.db_name = 'pets-api-test'
+        return create_app_base(
+            MONGODB_SETTINGS={'DB': self.db_name,
+                'HOST': MONGODB_HOST},
+            TESTING=True,
+            WTF_CSRF_ENABLED=False,
+            SECRET_KEY = 'mySecret!',
+        )
+
+    def setUp(self):
+        self.app_factory = self.create_app()
+        self.app = self.app_factory.test_client()
+
+    def tearDown(self):
+        db = _get_db()
+        db.client.drop_database(db)
+
+    def app_dict(self):
+        return json.dumps(dict(
+                app_id="pet_client",
+                app_secret="pet_secret"
+                ))
+
+    def store_dict(self):
+        return json.dumps(dict(
+            neighborhood="Bronxville",
+            street_address="1112 Bronxville Avenue",
+            city="Bronx",
+            state="NY",
+            zip="10567",
+            phone="718-222-2445"
+            ))
+
+    def pet_dict(self):
+        return json.dumps(dict(
+            name="Mac",
+            species="Dog",
+            breed="Shitzu",
+            age=11,
+            store=self.store_id,
+            price="855.22",
+            received_date="2016-11-11T12:12:01Z"
+            ))
+
+    def create_api_app(self):
+        # create our app
+        rv = self.app.post('/apps/',
+            data=self.app_dict(),
+            content_type='application/json')
+
+    def generate_access_token(self):
+        # generate an access token
+        rv = self.app.post('/apps/access_token/',
+            data=self.app_dict(),
+            content_type='application/json')
+        self.token = json.loads(rv.data.decode('utf-8')).get('token')
+
+    def headers(self):
+        return {
+            'X-APP-ID': 'pet_client',
+            'X-APP-TOKEN': self.token
+            }
+
+    def test_pets(self):
+        # get app up and running
+        self.create_api_app()
+        self.generate_access_token()
+
+        # create a store
+        rv = self.app.post('/stores/',
+            headers=self.headers(),
+            data=self.store_dict(),
+            content_type='application/json')
+        self.store_id = json.loads(rv.data.decode('utf-8')).get('store')['id']
+        assert rv.status_code == 201
+
+        # Create a Pet
+        rv = self.app.post('/pets/',
+            headers=self.headers(),
+            data=self.pet_dict(),
+            content_type='application/json')
+        pet_id = json.loads(rv.data.decode('utf-8')).get('pet')['id']
+        assert rv.status_code == 201
